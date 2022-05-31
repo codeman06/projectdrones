bf8c341bf4158226eee1a32e3deaf1ee872824ab
codeman06 <106576919+codeman06@users.noreply.github.com>
2022-05-31 11:45:53 +0200

    requirements new

--- kanie-drone/booking/settings.py
+++ kanie-drone/booking/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7!&zaz%o8++kvnqg#q%)fjph7)j6is8n)u&df3*eg_t#!70+19'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
